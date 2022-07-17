120dd34729d398426ea556c7f622156bbe9b200b
Olumide Ogundele <olumideralph@gmail.com>
2022-07-18 00:56:32 +0100

    implement multiplication in hack assembly code

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,43 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // set sum variable to 0
+    @sum
+    M=0
+
+    // initialize i (loop counter) to R1.
+    @R1
+    D=M
+    @i
+    M=D
+
+// start a loop that uses addition to achieve the multiplication.
+// if R0 = x and R1 = y, initialize the loop variable i = y,
+// keep decrementing till i == 0
+(LOOP)
+    // loop conditonal check, if i == 0
+    @i
+    // reduce i by 1.
+    D=M-1
+    M=D
+    @STOP
+    D; JLT
+
+    // loop body. (sum += R0)
+    @R0
+    D=M
+    @sum
+    M=D+M
+    // goto LOOP
+    @LOOP
+    0; JMP
+
+(STOP)
+    // set the value in variable sum to R2.
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0; JMP
