c519be1fc7aa3fe834c83f3b59da2e671ec1bf5a
Olumide Ogundele <olumideralph@gmail.com>
2022-07-20 23:04:40 +0100

    partial implementing of printing to the screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,51 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    @KBD
+    D=M
+    @input
+    M=0 // set the data input to white
+    @INNERLOOP
+    D; JEQ // if keyboard input is 0
+
+    @KBD
+    D=M
+    @input
+    M=-1 // set the data input to black
+    @INNERLOOP
+    D; JNE // if keyboard is not 0
+
+    @i
+    M=0
+    // inner loop.
+    (INNERLOOP)
+        @i
+        D=M
+        @SCREEN
+        D=A+D // pick the next memory/pixel
+
+        @input
+        A=D // address of next pixel.
+        // TODO: read from the keyboard input
+        // it currently sets all the screen pixel to black.
+        M=-1
+
+        // increment i
+        @i
+        M=M+1
+        D=M
+        @INNERLOOP
+        D; JGT
+
+        //TODO: create a base index to break
+        // out of the inner loop
+
+        // break out of the inner loop.
+        @END
+        0; JMP
+
+(END)
+    // go to the beginning of the loop.
+    @LOOP
+    0; JMP
