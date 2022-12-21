f17b1748f7ddecf1906287d3ca024f049ff1ff9f
Olumide Ogundele <olumideralph@gmail.com>
2022-12-21 09:20:44 +0000

    insert pre-created labels using enum

--- hack_assembler/src/assembler.rs
+++ hack_assembler/src/assembler.rs
@@ -3,6 +3,22 @@ use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::collections::HashMap;
 
+/// Pre-created labels in the symbol table.
+const SP: (&str, u8) = ("SP", 0);
+const LCL: (&str, u8) = ("LCL", 1);
+const ARG: (&str, u8) = ("ARG", 2);
+const THIS: (&str, u8) = ("THIS", 3);
+const THAT: (&str, u8) = ("THAT", 4);
+const SCREEN: (&str, u16) = ("SCREEN", 16384);
+const KBD: (&str, u16) = ("KBD", 24576); // Keyboard
+
+#[derive(Debug)]
+enum PreCreatedLabel {
+    Special((&'static str, u8)),
+    /// For SCREEN, KBD (Keyboard)
+    Devices((&'static str, u16)),
+}
+
 /// Assembler reads the hack assembly program using
 /// the provided path to the file.
 pub struct Assembler {
@@ -28,6 +44,27 @@ impl Assembler {
             self.symbol_table.insert(format!("R{value}"), value);
         }
 
+        let special_labels = vec![
+            PreCreatedLabel::Special(SP),
+            PreCreatedLabel::Special(LCL),
+            PreCreatedLabel::Special(ARG),
+            PreCreatedLabel::Special(THIS),
+            PreCreatedLabel::Special(THAT),
+            PreCreatedLabel::Devices(SCREEN),
+            PreCreatedLabel::Devices(KBD),
+        ];
+
+        // add special labels to the symbol table.
+       for label in &special_labels {
+           match label {
+               PreCreatedLabel::Special(label) => {
+                   self.symbol_table.insert(String::from(label.0), label.1 as u16)
+               },
+               PreCreatedLabel::Devices(label) => {
+                   self.symbol_table.insert(String::from(label.0), label.1)
+               },
+           };
+       }
         println!("{:?}", self.symbol_table);
     }
 
