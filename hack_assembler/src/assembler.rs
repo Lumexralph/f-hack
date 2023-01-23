cae3ea689edbdb75f22bb87a5e763e3f7483946e
Olumide Ogundele <olumideralph@gmail.com>
2023-01-23 09:07:04 +0000

    refactor ownership of file reader

--- hack_assembler/src/assembler.rs
+++ hack_assembler/src/assembler.rs
@@ -1,8 +1,9 @@
+use clap::builder::Str;
 use std::borrow::{Borrow, BorrowMut};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader, Read, Seek};
+use std::io::{BufRead, BufReader, Lines, Read, Seek};
 use std::ops::Index;
 use std::path::PathBuf;
 
@@ -38,7 +39,6 @@ lazy_static! {
 /// L_INSTRUCTION for (xxx), where xxx is a symbol.
 /// C_INSTRUCTION for instructions of this format dest=comp;jump.
 pub struct Parser<'a> {
-    file_reader: BufReader<File>,
     symbol_table: &'a mut HashMap<String, u16>,
     // Variable address starts from 16 and it is incremented
     // by 1 whenever another variable is encountered.
@@ -47,9 +47,8 @@ pub struct Parser<'a> {
 }
 
 impl<'a> Parser<'a> {
-    fn new(reader: BufReader<File>, table: &'a mut HashMap<String, u16>) -> Self {
+    fn new(table: &'a mut HashMap<String, u16>) -> Self {
         Parser {
-            file_reader: reader,
             symbol_table: table,
             variable_address: 16,
             instruction_line_no: 0,
@@ -58,26 +57,15 @@ impl<'a> Parser<'a> {
 
     // reset_file_reader rewinds the file buffer because it can be read by another
     // method and the file offset won't be at the beginning of the file.
-    fn reset_file_reader(&mut self) -> Result<(), io::Error> {
-        match self.file_reader.rewind() {
-            Ok(_) => Ok(()),
-            Err(err) => {
-                println!("error rewinding the file buffer {err}");
-                Err(err)
-            }
-        }
-    }
-
-    fn read_file(&self) -> (String, bool) {
-        let mut reader_iter = self.file_reader.lines();
-        match reader_iter.next() {
-            Some(Ok(content)) => (content, true),
-            None => ("".to_string(), false),
-            _ => {
-                panic!("something went wrong")
-            }
-        }
-    }
+    // fn reset_file_reader(&mut self) -> Result<(), io::Error> {
+    //     match self.file_reader.rewind() {
+    //         Ok(_) => Ok(()),
+    //         Err(err) => {
+    //             println!("error rewinding the file buffer {err}");
+    //             Err(err)
+    //         }
+    //     }
+    // }
 
     /// parse_labels() goes through the entire assembly program line by line,
     /// it keeps track of line number of code from 0 and is incremented by 1 whenever
@@ -86,111 +74,79 @@ impl<'a> Parser<'a> {
     /// It adds a new entry to the symbol table for label declaration (L_INSTRUCTION),
     /// associating the symbol with the current line number + 1 (this will be the ROM address
     /// of the next instruction in the program). No binary code is generated.
-    fn parse_labels(&mut self) {
-        if let Err(err) = self.reset_file_reader() {
-            // TODO: handle the error, propagate the error.
-            println!("reset_file_reader(): {err}");
+    fn parse_labels(&mut self, raw_content: &str) {
+        let mut content = String::from(raw_content.replace(" ", ""));
+        if content == "" || content.starts_with("//") {
+            return;
         }
 
-        let reader = &mut self.file_reader;
-        for line in reader.lines() {
-            match line {
-                Ok(mut content) => {
-                    // Ignore whitespaces and comments.
-                    content = String::from(content.replace(" ", ""));
-                    if content == "" || content.starts_with("//") {
-                        continue;
-                    }
+        // Remove in-line comments "//"
+        content = match content.split_once("//") {
+            Some((raw_content, _)) => raw_content.to_string(),
+            None => content,
+        };
 
-                    // Remove in-line comments "//"
-                    content = match content.split_once("//") {
-                        Some((raw_content, _)) => raw_content.to_string(),
-                        None => content,
-                    };
-
-                    // Handle L_INSTRUCTION
-                    if content.starts_with("(") && content.ends_with(")") {
-                        let label = &content[1..content.len() - 1];
-                        println!("{} L_INSTRUCTION: {label}", self.instruction_line_no + 1);
-                        self.symbol_table
-                            .insert(label.to_string(), self.instruction_line_no + 1);
-                    } else {
-                        // Assumes the remaining instructions are C  and A INSTRUCTIONS.
-                        self.instruction_line_no = self.instruction_line_no + 1;
-                    }
-                }
-                Err(error) => {
-                    println!("error reading line {}: {}", self.instruction_line_no, error);
-                }
-            }
+        // Handle L_INSTRUCTION
+        if content.starts_with("(") && content.ends_with(")") {
+            let label = &content[1..content.len() - 1];
+            println!("{} L_INSTRUCTION: {label}", self.instruction_line_no + 1);
+            self.symbol_table
+                .insert(label.to_string(), self.instruction_line_no + 1);
+        } else {
+            // Assumes the remaining instructions are C  and A INSTRUCTIONS.
+            self.instruction_line_no = self.instruction_line_no + 1;
         }
     }
 
     /// parse_instructions reads the entire assembly code again, it handles the
     /// A and C INSTRUCTIONS and generates the binary code that will be sent
     /// to the computer processor.
-    fn parse_instructions(&mut self) {
-        self.instruction_line_no = 0;
-        if let Err(err) = self.reset_file_reader() {
-            // TODO: handle the error, propagate the error.
-            println!("reset_file_reader(): {err}");
+    fn parse_instructions(&mut self, raw_content: &str) {
+        let mut content = String::from(raw_content.replace(" ", ""));
+        // Ignore whitespace, comment and labels (L_INSTRUCTIONS)
+        if content == "" || content.starts_with("//") || content.starts_with("(") {
+            return;
         }
 
-        let reader = &mut self.file_reader;
-        for line in reader.lines() {
-            match line {
-                Ok(val) => {
-                    let mut content = String::from(val.replace(" ", ""));
-                    // Ignore whitespace, comment and labels (L_INSTRUCTIONS)
-                    if content == "" || content.starts_with("//") || content.starts_with("(") {
-                        continue;
-                    }
+        // Remove in-line comments "//"
+        let refined_content = match content.split_once("//") {
+            Some((raw_content, _)) => raw_content,
+            None => content.as_str(),
+        };
+        content = refined_content.to_string();
 
-                    // Remove in-line comments "//"
-                    let refined_content = match content.split_once("//") {
-                        Some((raw_content, _)) => raw_content,
-                        None => content.as_str(),
-                    };
-                    content = refined_content.to_string();
-
-                    // Assumes only A and C INSTRUCTIONS are left after the
-                    // the ignored contents above i.e. comments, whitespace and labels.
-                    if content.starts_with("@") {
-                        //self.decode_a_instructions(content);
-                        continue;
-                    }
-
-                    // Possibly C-INSTRUCTION or invalid content.
-                    if content.contains("=") && RE.is_match(content.as_str()) {
-                        // Cut the dest part of content.
-                        match content.split_once("=") {
-                            Some((dest, remaining_substr)) => {
-                                println!("{} dest: {dest}", self.instruction_line_no);
-                                content = remaining_substr.to_string();
-                            }
-                            None => {}
-                        };
-                    }
-                    if content.contains(";") {
-                        match content.split_once(";") {
-                            Some((comp, jump)) => {
-                                println!("{} comp;jump => {comp};{jump}", self.instruction_line_no);
-                                content = comp.to_string();
-                            }
-                            None => {}
-                        };
-                    } else {
-                        // Assumes content will be comp if none
-                        // of the dest and jump conditions match.
-                        println!("comp: {content}");
-                    }
-                    self.instruction_line_no = self.instruction_line_no + 1;
-                }
-                Err(error) => {
-                    println!("error reading line {}: {}", self.instruction_line_no, error);
-                }
-            }
+        // Assumes only A and C INSTRUCTIONS are left after the
+        // the ignored contents above i.e. comments, whitespace and labels.
+        if content.starts_with("@") {
+            self.decode_a_instructions(content);
+            return;
         }
+
+        // Possibly C-INSTRUCTION or invalid content.
+        if content.contains("=") && RE.is_match(content.as_str()) {
+            // Cut the dest part of content.
+            match content.split_once("=") {
+                Some((dest, remaining_substr)) => {
+                    println!("{} dest: {dest}", self.instruction_line_no);
+                    content = remaining_substr.to_string();
+                }
+                None => {}
+            };
+        }
+        if content.contains(";") {
+            match content.split_once(";") {
+                Some((comp, jump)) => {
+                    println!("{} comp;jump => {comp};{jump}", self.instruction_line_no);
+                    content = comp.to_string();
+                }
+                None => {}
+            };
+        } else {
+            // Assumes content will be comp if none
+            // of the dest and jump conditions match.
+            println!("comp: {content}");
+        }
+        self.instruction_line_no = self.instruction_line_no + 1;
     }
 
     fn decode_a_instructions(&mut self, content: String) {
@@ -283,12 +239,32 @@ impl Assembler {
 
     pub fn read_file(&mut self) -> io::Result<()> {
         let f = File::open(&self.path)?;
-        // let reader = BufReader::new(f);
-        let parser = Parser::new(BufReader::new(f), &mut self.symbol_table);
-        // parser.parse_labels();
-        // parser.parse_instructions();
-        let (content, done) = parser.read_file();
-        println!("{content}, {done}");
+        let mut reader = BufReader::new(f);
+        let mut parser = Parser::new(&mut self.symbol_table);
+
+        // First pass.
+        for line in reader.by_ref().lines() {
+            match line {
+                Ok(content) => {
+                    parser.parse_labels(content.as_str());
+                }
+                Err(error) => println!("error reading file content"),
+            }
+        }
+
+        if let Err(result) = reader.rewind() {
+            return Err(result);
+        }
+
+        // Second pass.
+        for line in reader.by_ref().lines() {
+            match line {
+                Ok(content) => {
+                    parser.parse_instructions(content.as_str());
+                }
+                Err(error) => println!("error reading file content"),
+            }
+        }
 
         println!("{:?}", self.symbol_table);
         Ok(())
